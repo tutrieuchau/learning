e91a2ba627318bcb15fdcfbf9beecc02640da2bc
Nguyen Duc Tam <ductam@CV080011D-PC.cubevn.local>
2018-04-27 09:52:51 +0700

    hero detail add

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+  selectedHero:Hero;
   constructor() { }
-
   ngOnInit() {
+  }
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
 }
